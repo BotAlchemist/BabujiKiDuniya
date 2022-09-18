b9894b88cd1c6ebc52b12ac0465912804a82793c
Sumit Srivastava <sumvast@gmail.com>
2022-09-18 07:49:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 #translator = google_translator()  
 i_page= option_menu(menu_title= None, 
-                        options= ['Home', 'Chapters', 'Contact Us'],
+                        options= ['Home', 'Chapters', 'Contact'],
                         icons=['house-door-fill', 'book-fill', 'person-lines-fill'] ,
                         default_index=0, 
                         menu_icon= 'cast', 
@@ -38,33 +38,52 @@ if i_page== 'Home':
     
 
     page_context='''
-                                                            A Word
+                              A Word
 
-        All three parts on 'THE MASTER, THE MISSION AND THE METHOD' have constituted the book 'BABUJI'. 
-    First part of it presented to BABUJI on 30.4.1979 deals with his life history and Mission. 
-    Second part which was presented to him on 26.1.1985 explains as to how easily and effortlessly one can reach "BABUJI'S FEET". 
-    The third part which is being presented to "BABUJI'S FEET" today, the 30th April 1985 at the occasion of His 86th Birth ANNIVERSARY 
-    being celebrated at his birth place Shahajahanpur which would be the source of light to every soul of the universe describing the 
-    'Satpad' as spoken and stated from time to time by the MASTER.
+        All three parts on 'THE MASTER, THE MISSION AND THE METHOD' 
+    have constituted the book 'BABUJI'. 
+    First part of it presented to BABUJI on 30.4.1979 
+    deals with his life history and Mission. 
+    Second part which was presented to him on 26.1.1985 
+    explains as to how easily and effortlessly one can 
+    reach "BABUJI'S FEET". 
+    The third part which is being presented to 
+    "BABUJI'S FEET" today, the 30th April 1985 
+    at the occasion of His 86th Birth ANNIVERSARY 
+    being celebrated at his birth place Shahajahanpur 
+    which would be the source of light to every 
+    soul of the universe describing the 
+    'Satpad' as spoken and stated from 
+    time to time by the MASTER.
 
-    	Further this book consists of "BABUJI'S PARISH DECLARATION" and "BABUJI'S UNIVERSE". 
-    "BABUJI'S PARIS DECLARATION" indicates his future plan of Mission's work. 
-    And "BABUJI'S Universe" where in religion and spirituality have been described as 
-    the MASTER described which has been recorded by one of our organisers Shri U.N. Poddar, for the readers' interest. 
+    	Further this book consists of 
+    "BABUJI'S PARISH DECLARATION" and 
+    "BABUJI'S UNIVERSE". 
+    "BABUJI'S PARIS DECLARATION" indicates 
+    his future plan of Mission's work. 
+    And "BABUJI'S Universe" where in religion 
+    and spirituality have been described as 
+    the MASTER described which has been recorded 
+    by one of our organisers Shri U.N. Poddar, 
+    for the readers' interest. 
     
-    	My father is very grateful to shri Raghavendra Rao and Shri Khoosalbhai Patel for their inspiring letters enabling him to compile this book.
+    	My father is very grateful to 
+    shri Raghavendra Rao and 
+    Shri Khoosalbhai Patel for their inspiring 
+    letters enabling him to compile this book.
 
-                                            					DR. RAMESH KUMAR SAXENA, 
-                                                                                RADIOLOGIST,
-                                                                                RAMKRISHNA MISSION HOSPITAL,
-                                                                                ITANAGAR’ARUNACHAL PRADESH.
+                                            					
+                        DR. RAMESH KUMAR SAXENA,                                                                                 
+                        RADIOLOGIST,                                                                               
+                        RAMKRISHNA MISSION HOSPITAL,                                                                               
+                        ITANAGAR’ARUNACHAL PRADESH.
 
     
     '''
    
     
     page_context = GoogleTranslator(source='en', target= i_lang).translate(page_context)
-    st.markdown(page_context)
+    st.text(page_context)
     
     if play_button:
         output= gTTS(text= page_context, lang= i_lang)
